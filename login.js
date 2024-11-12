4c4b5148692904fbfdab334a0dbc7681b6f9df10
Reetesh prajapati <reeetshprajpati16@gmail.com>
2024-11-12 17:09:15 +0530

    add

--- login.js
+++ login.js
@@ -1,16 +1,16 @@
  
-var login = gsap.timeline();
+// var login = gsap.timeline();
 
-login.from("header , .logo, .item1 a, .cont> h3", {
-    y: -40,
-    opacity: 0,
-    duration: 1,
-    stagger: 0.2,
-});
-login.from(".container", {
-    opacity: 0,
-    duration: 1,
-});
+// login.from("header , .logo, .item1 a, .cont> h3", {
+//     y: -40,
+//     opacity: 0,
+//     duration: 1,
+//     stagger: 0.2,
+// });
+// login.from(".container", {
+//     opacity: 0,
+//     duration: 1,
+// });
 //  login.from("h1",{
 // y:-30,duration:.3,
 // opacity:0,
